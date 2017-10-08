afb2858f6cd083dcdb832498fb3e6dbfee96ed67
moon <evelkey@gmail.com>
2017-10-08 23:41:37 +0200

    working biconvLSTM

--- dynamic-segmentation/tf_fold/research.py
+++ dynamic-segmentation/tf_fold/research.py
@@ -23,6 +23,7 @@ from conv_lstm_cell import *
 # params
 EMBEDDING_SIZE = 64
 SEP = "|"
+BATCH_SIZE = 100
 data_dir = "/home/moon/data/"
 
 #our alphabet
@@ -35,57 +36,109 @@ index = lambda char: vocabulary.index(char)
 char = lambda i: vocabulary[i]
 
 
+
 # In[3]:
 
 
-def reader(queue, file):
-    """
-    read sentences from the data format setence: word\tword\n.....\t\n
-    """
-    while True:
-        with open(file) as f:
-            while True:
-                try:
-                    sentence = []
-                    while True:
-                        line = f.readline()[:-1].split('\t')
-                        if line[0] != "":
-                            sentence.append(line)
-                        else:
-                            break
-                    sent = " ".join([word[0] for word in sentence])
-                    segmented = " ".join([word[1].replace(" ","|") for word in sentence])
-                    tags = []
-                    last_char = "_"
-                    for char in segmented:
-                        if char != "|":
-                            tags.append(0 if last_char!="|" else 1)
-                        last_char = char
-                    queue.put((sent, tags))
-                except e:
-                    print(e)
-
-qsize= 10000
-pool = dict()
-data = dict()
-for dataset in ["train", "test", "validation"]:
-    data[dataset] = Queue(qsize)
-    pool[dataset] = Process(target=reader, args=(data[dataset], data_dir+dataset))
-    pool[dataset].start()
-    
-
-
-# In[4]:
-
-
-print(data["train"].get())
-
-#p.terminate()
+class reader():
+    def __init__(self, folder, mode="bidirectional", qsize=1000, start=True):
+        self.data_dir = folder
+        self.qsize= 10000
+        self.pool = dict()
+        self.data = dict()
+        self.datasets = ["train", "test", "validation"]
+        if mode == "bidirectional":
+            convert_fn = self.bidirectional_sentence_reader
+        else:
+            convert_fn = self.sentence_reader
+            
+        for dataset in self.datasets:
+            self.data[dataset] = Queue(qsize)
+            self.pool[dataset] = Process(target=convert_fn, args=(self.data[dataset], self.data_dir+dataset))
+        if start == True:
+            self.start()
+            
+    def sentence_reader(self, queue, file):
+        """
+        read sentences from the data format setence: word\tword\n.....\t\n
+        """
+        while True:
+            with open(file) as f:
+                while True:
+                    try:
+                        sentence = []
+                        while True:
+                            line = f.readline()[:-1].split('\t')
+                            if line[0] != "":
+                                sentence.append(line)
+                            else:
+                                break
+                        sent = " ".join([word[0] for word in sentence])
+                        segmented = " ".join([word[1].replace(" ","|") for word in sentence])
+                        tags = []
+                        last_char = "_"
+                        for char in segmented:
+                            if char != "|":
+                                tags.append(0 if last_char!="|" else 1)
+                            last_char = char
+                        queue.put((sent, tags))
+                    except e:
+                        print(e)
+                        
+    def bidirectional_sentence_reader(self, queue, file):
+        """
+        read sentences from the data format setence: word\tword\n.....\t\n
+        """
+        while True:
+            with open(file) as f:
+                while True:
+                    try:
+                        sentence = []
+                        while True:
+                            line = f.readline()[:-1].split('\t')
+                            if line[0] != "":
+                                sentence.append(line)
+                            else:
+                                break
+                        sent = " ".join([word[0] for word in sentence])
+                        segmented = " ".join([word[1].replace(" ","|") for word in sentence])
+                        tags = []
+                        last_char = "_"
+                        for char in segmented:
+                            if char != "|":
+                                tags.append(0 if last_char!="|" else 1)
+                            last_char = char
+                        for i in range(1, len(sent)-1):
+                            forward, backward = sent[:i], sent[i:][::-1]
+                            queue.put(([self.onehot(forward), self.onehot(backward)], tags[i]))
+                    except e:
+                        print(e)
+                        
+    def start(self):
+        for dataset in self.datasets:
+            self.pool[dataset].start()
+            
+    def stop(self):
+        for dataset in self.datasets:
+            self.pool[dataset].terminate()
+            
+    def get(self,dataset):
+        if dataset in self.datasets:
+            return self.data[dataset].get()
+        else:
+            raise KeyError
+            
+    def onehot(self, string):
+        onehot = np.zeros([len(string),vsize])
+        onehot[np.arange(len(string)), np.array([index(char) for char in string])]=1
+        return [onehot[i,:] for i in range(len(onehot))]
+            
+store = reader("/home/moon/data/", start=False)
 
 
 # ## helper functions
 
-# In[5]:
+# In[4]:
 
 
 def params_info():
@@ -104,101 +157,86 @@ def params_info():
     print(total_parameters)
     return total_parameters
 
-
-# In[6]:
-
-
-length = td.Length()
-
-embedded =  (td.InputTransform(lambda s: [index(x) for x in s]) >> 
-             td.Map(td.Scalar(tf.int32) >> 
-             td.Function(td.Embedding(vsize, EMBEDDING_SIZE))))
-
-onehot = (td.InputTransform(lambda s: [index(x) for x in s]) >>
-          td.Map(td.Scalar(tf.int32) >>
-          td.Function(lambda indices: tf.one_hot(indices, depth=vsize)) >>
-          td.Function(lambda x: tf.reshape(x, [-1,vsize,1]))))
-
-decode_onehot = td.InputTransform(lambda s: [char(np.argmax(np.squeeze(x))) for x in s])
-
-print(decode_onehot.eval(onehot.eval("malú")))
-#embedded.eval("kacsa")
+def onehot(string):
+    onehot = np.zeros([len(string),vsize])
+    onehot[np.arange(len(string)), np.array([index(char) for char in string])]=1
+    return [onehot[i,:] for i in range(len(onehot))]
 
 
-# In[7]:
+# In[5]:
 
 
-def conv1d_on_sequence(x, scope, kernel_size=3, input_channels=72, output_channels=72):
-    with tf.variable_scope(scope) as sc:
-        filters = tf.get_variable("conv_filter", [kernel_size] +  [input_channels, output_channels] , initializer=tf.truncated_normal_initializer(stddev=0.1))
-        bias = tf.get_variable("conv_bias",  output_channels, initializer=tf.constant_initializer(0.05, dtype=tf.float32))
-        conv = tf.nn.conv1d(x, filters=filters, stride=1, padding='VALID')
-        return tf.nn.relu(tf.add(conv, bias))
-    
-def SeqToTuple(T, N):
-    return (td.InputTransform(lambda x: tuple(x))
-            .set_input_type(td.SequenceType(T))
-            .set_output_type(td.Tuple(*([T] * N))))   
+cell = td.ScopedLayer(tf.contrib.rnn.BasicLSTMCell(num_units=16), 'char_cell')
+convlstm = Conv1DLSTMCell(input_shape=[vsize,1], output_channels=8, kernel_shape=[5])
+conv_lstm_cell_1d = td.ScopedLayer(convlstm)
+
+
+# In[38]:
+
+
+def bidirectional_conv_LSTM():
+    bidir_conv_lstm = td.Composition()
+    with bidir_conv_lstm.scope():
+        data = td.Record((td.Map(
+                                td.Vector(vsize) >>
+                                td.Function(lambda x: tf.reshape(x, [-1,vsize,1]))),
+                          td.Map(
+                                td.Vector(vsize) >>
+                                td.Function(lambda x: tf.reshape(x, [-1,vsize,1]))))).reads(bidir_conv_lstm.input)
+
+        forward = td.Identity().reads(data[0])
+        backward = td.Identity().reads(data[1])
+
+        forw = (td.RNN(conv_lstm_cell_1d) >>
+                td.GetItem(1) >>
+                td.GetItem(0) >>
+                td.Function(lambda rnn_outs: tf.contrib.layers.flatten(rnn_outs))).reads(forward)
+
+        backw = (td.RNN(conv_lstm_cell_1d) >>
+                 td.GetItem(1) >>
+                 td.GetItem(0) >>
+                 td.Function(lambda rnn_outs: tf.contrib.layers.flatten(rnn_outs))).reads(backward)
+
+        rnn_outs = td.Concat().reads(forw,backw)
+        bidir_conv_lstm.output.reads(rnn_outs)
+    return bidir_conv_lstm
+
+
+def FCNN():
+    return td.FC(400) >> td.FC(50) >> td.FC(1)
+
+bidir = bidirectional_conv_LSTM()
+fc = FCNN()
+blk = bidir >> fc 
+#blk.eval([a,a])
+
+
+# In[39]:
+
+
+compiler = td.Compiler.create((blk, td.Scalar()))
+model_output, target = compiler.output_tensors
+loss = tf.nn.l2_loss(model_output - target)
+train_op = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(loss)
 
 
 # In[8]:
 
 
-convlstm = Conv1DLSTMCell(input_shape=[vsize,1], output_channels=8, kernel_shape=[5])
-conv_lstm_cell_1d = td.ScopedLayer(convlstm)
+a = onehot("asa pssdf sadf adsf adfsad")
+b = onehot("trhko daf  sdf sd")
+blk.eval([a,a])
+#store.stop()
 
 
-# In[9]:
+# In[69]:
 
 
-comp = td.Composition()
-with comp.scope():
-    #forward = td.Identity().reads(comp.input[0])
-    #backward = td.Identity().reads(comp.input[1])
-    model1 = (td.InputTransform(lambda s: [index(x) for x in s]) >>
-             td.Map(td.Scalar(tf.int32) >>
-             td.Function(lambda indices: tf.one_hot(indices, depth=vsize)) >>
-             td.Function(lambda x: tf.reshape(x, [-1,vsize,1]))) >>             #onehot encoding
-             td.RNN(conv_lstm_cell_1d)  >> td.GetItem(1) >> td.GetItem(0) ).reads(comp.input)
-    model2 = (td.InputTransform(lambda s: [index(x) for x in s]) >>
-             td.Map(td.Scalar(tf.int32) >>
-             td.Function(lambda indices: tf.one_hot(indices, depth=vsize)) >>
-             td.Function(lambda x: tf.reshape(x, [-1,vsize,1]))) >>             #onehot encoding
-             td.RNN(conv_lstm_cell_1d) >> td.GetItem(1) >> td.GetItem(0) ).reads(comp.input)
-    
-    rnn_outs = td.Concat().reads(model1, model2)
-    out = td.Function(lambda rnn_outs: tf.contrib.layers.flatten(rnn_outs)).reads(rnn_outs)
-    #fc = out >> td.FC(300) >> td.FC(1)
-    comp.output.reads(out)
-    
-new = comp >> td.FC(300) >>td.FC(200) >> td.FC(30) >> td.FC(1, activation=tf.nn.sigmoid)
-
-
-# In[14]:
-
-
-#new.eval(("cucc", "valami")).shape
-compiler = td.Compiler.create(new)
-(model_output,) = compiler.output_tensors
-loss = tf.nn.l2_loss(model_output)
-train_op = tf.train.AdamOptimizer(0.001).minimize(loss)
-
-
-# In[13]:
-
-
-#print(new.eval("ss"))
-print(new.input_type, new.output_type)
-#sess.run(tf.global_variables_initializer())
-#compiler.loom_input_tensor
-#sess.run(tf.local_variables_initializer())
-#sess.run(train_op,{compiler.loom_input_tensor: ["aa","bb"]})
+sess.run(tf.global_variables_initializer())
 
 
 # In[ ]:
 
 
-fd = compiler.build_feed_dict(["nyam","mam", "ouf"])
-sess.run(tf.global_variables_initializer())
-sess.run(train_op, feed_dict=fd)
+sess.run([loss, train_op], compiler.build_feed_dict([([b,b], 1.0) for _ in range(5)]))
 
